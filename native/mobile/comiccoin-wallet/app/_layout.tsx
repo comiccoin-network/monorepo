864f680a5aa39621bfba7be7e03ad6cec41010c6
Bartlomiej Mika <bart@mikasoftware.com>
2025-01-27 22:50:13 -0500

    Added internet connectivity checking GUI to popup if no internet occurs.

--- native/mobile/comiccoin-wallet/app/_layout.tsx
+++ native/mobile/comiccoin-wallet/app/_layout.tsx
@@ -1,10 +1,11 @@
 // monorepo/native/mobile/comiccoin-wallet/app/_layout.tsx
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
+import InternetProvider from "../providers/InternetProvider";
 
 export default function RootLayout() {
   return (
-    <>
+    <InternetProvider>
       <StatusBar style="dark" />
       <Stack
         screenOptions={{
@@ -14,6 +15,6 @@ export default function RootLayout() {
           },
         }}
       />
-    </>
+    </InternetProvider>
   );
 }
