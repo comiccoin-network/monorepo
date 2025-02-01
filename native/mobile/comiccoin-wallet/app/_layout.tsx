9da4f7505a462e6be0345bbb34866e1a079328fc
Bartlomiej Mika <bart@mikasoftware.com>
2025-02-01 14:38:17 -0500

    Removed deprecated code

--- native/mobile/comiccoin-wallet/app/_layout.tsx
+++ native/mobile/comiccoin-wallet/app/_layout.tsx
@@ -2,22 +2,19 @@
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import InternetProvider from "../providers/InternetProvider";
-import { BlockchainProvider } from "../contexts/BlockchainStateContext";
 
 export default function RootLayout() {
   return (
     <InternetProvider>
-      <BlockchainProvider chainId={1}>
-        <StatusBar style="dark" />
-        <Stack
-          screenOptions={{
-            headerShown: false,
-            contentStyle: {
-              backgroundColor: "transparent",
-            },
-          }}
-        />
-      </BlockchainProvider>
+      <StatusBar style="dark" />
+      <Stack
+        screenOptions={{
+          headerShown: false,
+          contentStyle: {
+            backgroundColor: "transparent",
+          },
+        }}
+      />
     </InternetProvider>
   );
 }
