aa4839b3f8211c25ee654f8f0ccffb5480ad5bee
Bartlomiej Mika <bart@mikasoftware.com>
2025-02-06 01:21:31 -0500

    Minor edits

--- native/desktop/comiccoin-wallet/main.go
+++ native/desktop/comiccoin-wallet/main.go
@@ -67,8 +67,8 @@ func main() {
 	// Create application with options
 	err := wails.Run(&options.App{
 		Title:  "ComicCoin Wallet",
-		Width:  1024,
-		Height: 768,
+		Width:  1440,
+		Height: 900,
 		AssetServer: &assetserver.Options{
 			Assets:  assets,
 			Handler: NewFileLoader(), // https://wails.io/docs/guides/dynamic-assets/
