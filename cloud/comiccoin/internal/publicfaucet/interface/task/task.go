6b9e31796379936e302282b0da331b71406d93b7
Bartlomiej Mika <bart@mikasoftware.com>
2025-02-24 12:41:11 -0500

    Update task

--- cloud/comiccoin/internal/publicfaucet/interface/task/task.go
+++ cloud/comiccoin/internal/publicfaucet/interface/task/task.go
@@ -36,6 +36,23 @@ func NewTaskManager(
 
 func (port *taskManagerImpl) Run() {
 	port.logger.Info("Running Task Manager")
+
+	//
+	// STEP 1:
+	// When task running begins, let's fetch from authority.
+	//
+
+	if err := port.updateFaucetBalanceByAuthorityTask.Execute(context.Background()); err != nil {
+		port.logger.Error("Failed running remote account balance sync - Trying again in 10 seconds...",
+			slog.Any("error", err))
+		time.Sleep(10 * time.Second)
+	}
+
+	//
+	// Step 2:
+	// Run in background to fetch latest faucet balance.
+	//
+
 	for {
 		if err := port.updateFaucetBalanceByAuthorityTask.Execute(context.Background()); err != nil {
 			port.logger.Error("Failed running remote account balance sync - Trying again in 10 seconds...",
@@ -43,8 +60,8 @@ func (port *taskManagerImpl) Run() {
 			time.Sleep(10 * time.Second)
 			continue
 		}
-		time.Sleep(10 * time.Second)
-		port.logger.Debug("Finished sync'ing remote account balance, will sync again...")
+		time.Sleep(1 * time.Hour)
+		port.logger.Debug("Finished sync'ing remote account balance, will sync again in 1 hour...")
 	}
 }
 
