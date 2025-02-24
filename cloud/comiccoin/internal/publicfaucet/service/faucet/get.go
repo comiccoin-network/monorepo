3076ed96acd6daaa8c7d156a80e01aeac7329448
Bartlomiej Mika <bart@mikasoftware.com>
2025-02-24 12:47:47 -0500

    BUGFIX: Fixed the field types for faucet.

--- cloud/comiccoin/internal/publicfaucet/service/faucet/get.go
+++ cloud/comiccoin/internal/publicfaucet/service/faucet/get.go
@@ -15,14 +15,15 @@ import (
 type FaucetDTO struct {
 	ChainID                    uint16    `bson:"chain_id" json:"chain_id"`
 	Balance                    uint64    `bson:"balance" json:"balance"`
-	UsersCount                 uint16    `bson:"users_count" json:"users_count"`
+	UsersCount                 uint64    `bson:"users_count" json:"users_count"`
 	TotalCoinsDistributed      uint64    `bson:"total_coins_distributed" json:"total_coins_distributed"`
-	TotalTransactions          uint16    `bson:"total_transactions" json:"total_transactions"`
-	DistributationRatePerDay   uint16    `bson:"distribution_rate_per_day" json:"distribution_rate_per_day"`
-	TotalCoinsDistributedToday uint16    `bson:"total_coins_distributed_today" json:"total_coins_distributed_today"`
-	TotalTransactionsToday     uint16    `bson:"total_transactions_today" json:"total_transactions_today"`
+	TotalTransactions          uint64    `bson:"total_transactions" json:"total_transactions"`
+	DistributationRatePerDay   uint64    `bson:"distribution_rate_per_day" json:"distribution_rate_per_day"`
+	TotalCoinsDistributedToday uint64    `bson:"total_coins_distributed_today" json:"total_coins_distributed_today"`
+	TotalTransactionsToday     uint64    `bson:"total_transactions_today" json:"total_transactions_today"`
 	CreatedAt                  time.Time `bson:"created_at,omitempty" json:"created_at,omitempty"`
 	LastModifiedAt             time.Time `bson:"last_modified_at,omitempty" json:"last_modified_at,omitempty"`
+	DailyCoinsReward           uint64    `bson:"daily_coins_reward" json:"daily_coins_reward"`
 }
 
 type GetFaucetService interface {
@@ -92,5 +93,6 @@ func (svc *getFaucetServiceImpl) ExecuteByChainID(ctx context.Context, chainID u
 		TotalTransactionsToday:     faucet.TotalTransactionsToday,
 		CreatedAt:                  faucet.CreatedAt,
 		LastModifiedAt:             faucet.LastModifiedAt,
+		DailyCoinsReward:           svc.config.Blockchain.PublicFaucetClaimCoinsReward,
 	}, nil
 }
