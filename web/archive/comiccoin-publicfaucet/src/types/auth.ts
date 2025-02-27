40e85154f1dfe58c56e386668150651686e38418
Bartlomiej Mika <bart@mikasoftware.com>
2025-02-27 00:33:04 -0500

    🚮 Removed deprecated codebase which was the `next.js` written project.

--- web/archive/comiccoin-publicfaucet/src/types/auth.ts
+++
@@ -1,7 +0,0 @@
-// github.com/comiccoin-network/monorepo/web/comiccoin-publicfaucet/src/types/auth.ts
-//
-export interface Tokens {
-  accessToken: string;
-  refreshToken: string;
-  expiresAt: number;
-}
