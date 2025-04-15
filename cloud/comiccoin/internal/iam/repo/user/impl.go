0976c8354523547c22225eaef761c52e61196129
Bartlomiej Mika <bart@mikasoftware.com>
2025-04-14 22:28:19 -0400

    Improved performance.

--- cloud/comiccoin/internal/iam/repo/user/impl.go
+++ cloud/comiccoin/internal/iam/repo/user/impl.go
@@ -7,6 +7,7 @@ import (
 
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 
 	"github.com/comiccoin-network/monorepo/cloud/comiccoin/config"
 	dom_user "github.com/comiccoin-network/monorepo/cloud/comiccoin/internal/iam/domain/user"
@@ -22,13 +23,13 @@ func NewRepository(appCfg *config.Configuration, loggerp *slog.Logger, client *m
 	// ctx := context.Background()
 	uc := client.Database(appCfg.DB.IAMName).Collection("users")
 
-	// // For debugging purposes only or if you are going to recreate new indexes.
-	// if _, err := uc.Indexes().DropAll(context.TODO()); err != nil {
-	// 	loggerp.Warn("failed deleting all indexes",
-	// 		slog.Any("err", err))
-	//
-	// 	// Do not crash app, just continue.
-	// }
+	// For debugging purposes only or if you are going to recreate new indexes.
+	if _, err := uc.Indexes().DropAll(context.TODO()); err != nil {
+		loggerp.Warn("failed deleting all indexes",
+			slog.Any("err", err))
+
+		// Do not crash app, just continue.
+	}
 
 	// Note:
 	// * 1 for ascending
@@ -38,13 +39,13 @@ func NewRepository(appCfg *config.Configuration, loggerp *slog.Logger, client *m
 	// The following few lines of code will create the index for our app for this
 	// collection.
 	_, err := uc.Indexes().CreateMany(context.TODO(), []mongo.IndexModel{
-		// {
-		// 	Keys:    bson.D{{Key: "federatedidentity_id", Value: -1}},
-		// 	Options: options.Index().SetUnique(true),
-		// },
 		{Keys: bson.D{
 			{Key: "created_at", Value: -1},
 		}},
+		{
+			Keys:    bson.D{{Key: "email", Value: -1}},
+			Options: options.Index().SetUnique(true),
+		},
 		{Keys: bson.D{
 			{Key: "status", Value: 1},
 			{Key: "created_at", Value: -1},
